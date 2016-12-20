5c50f378063688d85675428c895890be65d2b8e2
gempurbayu <aji123zz@gmail.com>
2016-12-20 11:13:28 +0700

    tampilan bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
